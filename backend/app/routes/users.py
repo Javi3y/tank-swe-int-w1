d6803f456117baca0a2767167a551fa9d89cb03b
javi3y <javieyprograms@gmail.com>
2025-01-27 11:44:29 +0330

    ignore __pycache__(yep i still hate that mf)

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -2,9 +2,10 @@ from typing import List
 from fastapi import APIRouter
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app import models, schemas
+from app import schemas
 from app.database import get_db
 from fastapi import Depends
+from app.models import users
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -12,5 +13,6 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/", response_model=List[schemas.UserOut])
 async def get_users(db: AsyncSession = Depends(get_db)):
-    users = await db.execute(select(models.User))
-    return users.scalars().all()
+
+    all_users = await db.execute(select(users.User))
+    return all_users.scalars().all()
