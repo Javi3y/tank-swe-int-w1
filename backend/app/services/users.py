01009af9f58f02063138e8d7534df45edb503080
javi3y <javieyprograms@gmail.com>
2025-01-27 12:15:09 +0330

    added users service

---
+++ backend/app/services/users.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models import users
+from app.schemas import UserOut
+from typing import Annotated, List
+
+class UserService:
+    async def get_items(self, db: AsyncSession) -> List[UserOut]:
+        all_users = await db.execute(select(users.User))
+        return all_users.scalars().all()
+
+async def get_user_service() -> UserService:
+    return UserService()
+
+user_service_dependency = Annotated[UserService, Depends(get_user_service)]
