248cd4808640e00ff4f59250d797384a39fa5e70
javi3y <javieyprograms@gmail.com>
2025-01-30 12:02:17 +0330

    wait a minute i might revert

--- backend/app/models/purchase.py
+++ backend/app/models/purchase.py
@@ -1,6 +1,6 @@
 from app.database import Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Boolean, Column, ForeignKey, DateTime, Integer, text
+from sqlalchemy import Boolean, Column, ForeignKey, DateTime, text
 
 from sqlalchemy.sql.functions import current_timestamp
 
@@ -13,7 +13,11 @@ class Reservation(Base):
     book = relationship("Book")
     start = Column("start", DateTime(), default=current_timestamp(), nullable=False)
     end = Column("end", DateTime(), default=current_timestamp(), nullable=False)
-    price = Column(Integer, nullable=False)
-    position = Column(Integer, nullable=False, server_default=text("0"))
-    is_satisfied = Column(Boolean, nullable=False, server_default=text("false"))
-    is_done = Column(Boolean, nullable=False, server_default=text("false"))
+    is_ended = Column("is_ended", Boolean(), server_default=text('flase'), nullable=False)
+
+class ReservationQueue(Base):
+    __tablename__= "reservation_queue"
+    client_id = Column(ForeignKey("client.id", ondelete="CASCADE"), nullable=False)
+    client = relationship("Client")
+    book_id = Column(ForeignKey("book.id", ondelete="CASCADE"), nullable=False)
+    book = relationship("Book")
